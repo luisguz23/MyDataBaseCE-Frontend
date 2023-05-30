7a3ea5ced7f742866b5c7a6d70810955f51442e5
luisguz23 <luguzman@estudiantec.cr>
2023-05-29 18:18:11 -0600

    Se agregan componentes para la vista de arbol de los archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyDataBaseCE';
+
+  title = 'Frontend';
+  public treeviewData : Object [] = [
+    {id: '01', name: 'Local Disk (C:)'},
+    {id: '02', name: 'Local Disk (D:)'},
+    {id: '03', name: 'Local Disk (E:)'}
+  ];
+
+  public datasourceFields : Object = {
+    dataSource : this.treeviewData, id: 'id', text: 'name'
+  }
+
+  
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TreeViewModule } from '@syncfusion/ej2-angular-navigations';
+import { ConexionComponent } from './conexion/conexion.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConexionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    TreeViewModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
